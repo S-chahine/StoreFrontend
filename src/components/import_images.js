0e5b3ee82868b55e7f9e448cd0f242dbd40044ec
S-chahine <samah_chahine@outlook.com>
2024-01-30 09:01:53 -0500

    CSS modifications

--- src/components/import_images.js
+++ src/components/import_images.js
@@ -6,20 +6,20 @@ import skirts from '../public/assets/images/categories/skirts.jpg';
 import pants from '../public/assets/images/categories/pants.jpg';
 
 export const import_images = (imageURL) => {
-        switch (imageURL) {
-          case 'basics':
-            return basics;
-          case 'outerwear':
-            return outerwear;
-          case 'tops':
-            return tops;
-          case 'dresses':
-            return dresses;
-          case 'skirts':
-            return skirts;
-          case 'pants':
-            return pants;
-          default:
-            return '';
-        }
-      };
+  switch (imageURL) {
+    case 'basics':
+      return basics;
+    case 'outerwear':
+      return outerwear;
+    case 'tops':
+      return tops;
+    case 'dresses':
+      return dresses;
+    case 'skirts':
+      return skirts;
+    case 'pants':
+      return pants;
+    default:
+      return '';
+  }
+};
